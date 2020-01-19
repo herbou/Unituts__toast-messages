2ff684130f905e6288756be4bd2a63b0a0934543
hamza herbou <35342314+herbou@users.noreply.github.com>
2020-01-20 00:55:04 +0100

    Add files via upload

---
+++ Toast_Unity/Assets/Demo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+
+	public void button1Clicked ()
+	{
+		Toast.Instance.Show ("Hello");
+	}
+
+	public void button2Clicked ()
+	{
+		Toast.Instance.Show ("Hello 2s", 2f);
+	}
+
+	public void button3Clicked ()
+	{
+		Toast.Instance.Show ("Hello 2s color:magenta", 3f, Toast.ToastColor.Magenta);
+	}
+}
